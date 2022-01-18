b8500782c3ca8c801b59dc9f50ef5db0f9559c39
BillWhoever <vasmpeslemes@gmail.com>
2022-01-18 19:30:06 +0200

    initial

---
+++ Part2Data/McPATexec.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+from optparse import OptionParser
+import os
+import sys
+import re
+import json
+import types
+import math
+from xml.etree import ElementTree as ET
+
+benchmarks = ["401bzip2", "429mcf", "456hmmer", "458sjeng", "470lbm"]
+runs = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]
+	
+for x in benchmarks:
+	for y in runs:
+		os.system("./mcpat/mcpat -infile ./McPatInput/{}{}.xml -print_level 5 > ./McPatOutput/results{}{}.txt".format(x, y, x, y) )
+print('process is completed')
---
+++ Part2Data/conversionCaller.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+from optparse import OptionParser
+import os
+import sys
+import re
+import json
+import types
+import math
+from xml.etree import ElementTree as ET
+
+benchmarks = ["401bzip2", "429mcf", "456hmmer", "458sjeng", "470lbm"]
+runs = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]
+	
+for x in benchmarks:
+	for y in runs:
+		os.system("python2 GEM5ToMcPAT.py -o ./McPatInput/{}{}.xml ./gem5Output/{}/{}/stats.txt ./Inputs/{}/{}/config.json inorder_arm.xml".format(x, y, x, y, x, y) )
+print('process is completed')
+
