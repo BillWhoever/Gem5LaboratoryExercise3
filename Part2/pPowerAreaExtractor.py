33edca77f4b49e96c37a52c0552395fc1f794d21
BillWhoever <95180387+BillWhoever@users.noreply.github.com>
2022-01-20 20:31:07 +0200

    Update pPowerAreaExtractor.py

--- Part2/pPowerAreaExtractor.py
+++ Part2/pPowerAreaExtractor.py
@@ -11,29 +11,29 @@ from xml.etree import ElementTree as ET
 benchmarks = ["401bzip2", "429mcf", "456hmmer", "458sjeng", "470lbm"]
 runs = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]
 
-with open("dataOutputB.txt", "a") as o:
+with open("dataOutputB.txt", "a") as o:							#opens the dataOutputB.txt file to write
 	for y in runs:
 		o.write("Machine #{}".format(y) )
 		o.write("\n")
 		for x in benchmarks:
 			o.write("Benchmark:-{}- CPI:-".format(x) )
-			a_file = open("gem5Output/{}/{}/stats.txt".format(x, y))	#opens the correct stats.txt file
+			a_file = open("gem5Output/{}/{}/stats.txt".format(x, y))	#opens the correct stats.txt file to read
 			lines_to_read = [28]
 			for position, line in enumerate(a_file):
 				if position in lines_to_read:
-					line = line [44 : 52]				#prints the IPC
-					o.write(line)
-			a_file = open("McPatOutput/results{}{}.txt".format(x, y))	#opens the correct resultsxx.txt file
+					line = line [44 : 52]
+					o.write(line)					#prints the IPC
+			a_file = open("McPatOutput/results{}{}.txt".format(x, y))	#opens the correct resultsxx.txt file to read
 			lines_to_read = [15]
 			for position, line in enumerate(a_file):
 				if position in lines_to_read:
 					line = line [14 : -1]				
 					o.write("- PeakPower:-{}".format(line) )	#print Peak Power
-			a_file = open("McPatOutput/results{}{}.txt".format(x, y))	#opens the correct resultsxx.txt file
+			a_file = open("McPatOutput/results{}{}.txt".format(x, y))	#opens the correct resultsxx.txt file to read
 			lines_to_read = [14]
 			for position, line in enumerate(a_file):
 				if position in lines_to_read:
 					line = line [8 : -1]				
-					o.write("-Area:-{}".format(line) )	#print Area
+					o.write("-Area:-{}".format(line) )		#print Area
 			o.write("\n")
 	print('process is completed')
