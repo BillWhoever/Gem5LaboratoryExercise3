cde86deabd437c89f62791832b9a54fe48130d08
BillWhoever <95180387+BillWhoever@users.noreply.github.com>
2022-01-20 20:37:35 +0200

    Update dataExtractor.py

--- Part2/dataExtractor.py
+++ Part2/dataExtractor.py
@@ -18,29 +18,29 @@ with open("dataOutput.txt", "a") as o:
 		for x in benchmarks:
 			o.write("\n")
 			o.write("- Benchmark: {} - CPI = ".format(x) )
-			a_file = open("gem5Output/{}/{}/stats.txt".format(x, y))
+			a_file = open("gem5Output/{}/{}/stats.txt".format(x, y))		#opens the correct stats.txt file
 			lines_to_read = [28]
 			for position, line in enumerate(a_file):
 				if position in lines_to_read:
-					line = line [44 : 52]
+					line = line [44 : 52]					#prints the IPC
 					o.write(line)
 			o.write("-")
-			a_file = open("EnergyPrinterOutput/output{}{}.txt".format(x, y))
+			a_file = open("EnergyPrinterOutput/output{}{}.txt".format(x, y))	#opens the correct outputxx.txt file to write
 			lines_to_read = [3]
 			for position, line in enumerate(a_file):
 				if position in lines_to_read:
 					o.write(line)
-			a_file = open("McPatOutput/results{}{}.txt".format(x, y))
-			lines_to_read = [61, 66, 63, 65]
+			a_file = open("McPatOutput/results{}{}.txt".format(x, y))		#opens the correct resultsxx.txt file to read
+			lines_to_read = [61, 66, 63, 65]					#print the Core data
 			o.write ("- Core Data -")
 			o.write("\n")
 			for position, line in enumerate(a_file):
 				if position in lines_to_read:
 					o.write(line)
-			a_file = open("McPatOutput/results{}{}.txt".format(x, y))
+			a_file = open("McPatOutput/results{}{}.txt".format(x, y))		#reopens the correct resultsxx.txt file to read
 			o.write ("- L2 Data -")
 			o.write("\n")
-			lines_to_read = [277, 282, 279, 281]
+			lines_to_read = [277, 282, 279, 281]					#print the L2 data
 			for position, line in enumerate(a_file):
 				if position in lines_to_read:
 					o.write(line)
