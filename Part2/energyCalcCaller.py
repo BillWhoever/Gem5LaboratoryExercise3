35a49d700c07700834a0861f60bab15b214ad515
BillWhoever <95180387+BillWhoever@users.noreply.github.com>
2022-01-20 20:37:29 +0200

    Update energyCalcCaller.py

--- Part2/energyCalcCaller.py
+++ Part2/energyCalcCaller.py
@@ -11,9 +11,9 @@ from xml.etree import ElementTree as ET
 benchmarks = ["401bzip2", "429mcf", "456hmmer", "458sjeng", "470lbm"]
 runs = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]
 	
-for x in benchmarks:
-	for y in runs:
-		os.system("echo --- RESULTS FOR Benchmark:{} and Run#:{} --- FINISHED!".format(x, y, x, y) )
-		os.system("python2 print_energy.py ./McPatOutput/results{}{}.txt ./gem5Output/{}/{}/stats.txt > ./Output/output{}{}.txt".format(x, y, x, y, x, y) )
+for x in benchmarks:		#Benchmark loop
+	for y in runs:		#Run loop (# of machine tested)
+		os.system("echo --- RESULTS FOR Benchmark:{} and Run#:{} --- FINISHED!".format(x, y, x, y) )		#console only printer
+		os.system("python2 print_energy.py ./McPatOutput/results{}{}.txt ./gem5Output/{}/{}/stats.txt > ./Output/output{}{}.txt".format(x, y, x, y, x, y) )	#data printed on the correct output file
 print('process is completed')
 
